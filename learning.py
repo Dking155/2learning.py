3ae7620c08301848b31c673b7c5dca4892417368
100134969 <kingpatrick455@gmail.com>
2019-09-30 09:02:29 -0500

    playing with the maths and the octothorpes

--- learning.py
+++ learning.py
@@ -3,3 +3,11 @@
 print("howdy y'all")
 print("I like typing this.")
 print("This is fun.")
+
+# math skills demo
+
+print("I will not count my chickens", 2)
+print("Hens: ", 25+30/6)
+print("Roosters: ", 100-25*3 % 4)
+print("Now I will count the eggs", 7)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
