5dbbcb8ba0a7ec8bdf530d948afbc57d84c8fbeb
100134969 <kingpatrick455@gmail.com>
2019-10-01 08:31:19 -0500

    Explaining

--- learning.py
+++ learning.py
@@ -1,11 +1,14 @@
 # this is an octhorpe--it makes a comment (line ignored by the computer)
 # print("hello world")
+#It makes it say howdy yall at the bottom
 print("howdy y'all")
+# makes it say I like typing this at the bottom
 print("I like typing this.")
+#it makes it say this is fun at the bottom
 print("This is fun.")
 
 # math skills demo
-
+#This sets up all the math
 print("I will not count my chickens", 2)
 print("Hens: ", 25+30/6)
 print("Roosters: ", 100-25*3 % 4)
